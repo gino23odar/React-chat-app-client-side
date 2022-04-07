3310027696418931e790326fd9762df7be0dd224
Gino <ginoalodar@gmail.com>
2022-04-07 18:12:19 -0500

    Added the initial structure for a side bar

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import {ChannelList, useChatContext} from 'stream-chat-react';
+import Cookies from 'universal-cookie';
+
+import {ChannelSearch, TeamChannelList, TeamchannelPreview} from './';
+import ChatIcon from '../assets/chaticon.png';
+import LogoutIcon from '../assets/logouticon.jpg';
+
+const SideBar = ()=>(
+  <div className='channel-list__sidebar'>
+    <div className ='channel-list__sidebar__icon1'>
+      <div className='icon1__inner'>
+        <img src = {ChatIcon} alt='Icon' width='30'/>
+      </div>
+    </div>
+    <div className ='channel-list__sidebar__icon2'>
+      <div className='icon1__inner'>
+        <img src = {LogoutIcon} alt='Logout' width='30'/>
+      </div>
+    </div>
+  </div>
+);
 
 const ChannelListContainer = () => {
   return (
-    <div>
-      ChannelListContainer
-    </div>
+    <>
+      <SideBar/>
+    </>
   );
 }
 
