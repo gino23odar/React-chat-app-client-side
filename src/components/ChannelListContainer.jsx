d32de9f44644127ea19af9831cc506f1c339a30c
Gino <ginoalodar@gmail.com>
2022-04-09 18:22:26 -0500

    added base to fetch team Lists

--- src/components/ChannelListContainer.jsx
+++ src/components/ChannelListContainer.jsx
@@ -35,6 +35,10 @@ const ChannelListContainer = () => {
       <div className='channel-list__list__wrapper'>
         <CompanyHeader/>
         <ChannelSearch/>
+        <ChannelList filters={{}} 
+          channelRenderFilterFn={()=>{}} 
+          List={(listProps) =>(<TeamChannelList {...listProps} type='team'/>)}
+        />
       </div>
     </>
   );
