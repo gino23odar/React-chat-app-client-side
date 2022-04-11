36891c8e85174c3e7276edcc47c73af00b268759
Gino <ginoalodar@gmail.com>
2022-04-10 22:31:34 -0500

    built layout for the login on the client side

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,17 +2,68 @@ import React, {useState} from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
 
-import signinImage from '../assets/signup.png';
+import signinImage from '../assets/signup.jpg';
 
 const Auth = () => {
-  const [isSignup, setIsSignup] = useState(false);
+  const [isSignup, setIsSignup] = useState(true);
+
+  const handleChange=()=>{}
+
+  const switchMode = () =>{
+    setIsSignup((prevIsSignup)=> !prevIsSignup);
+  }
 
   return (
     <div className='auth__form-container'>
       <div className='auth__form-container_fields'>
         <div className='auth__form-container_fields-content'>
           <p>{isSignup? 'Sign Up' : 'Sign In'}</p>
+          <form onSubmit={()=>{}}>
+            {isSignup && (
+              <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='fullName'>Full Name</label>
+                <input name='fullName' type='text' placeholder='Full Name' onChage={handleChange} required/>
+              </div>
+            )}
+            <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='username'>Username</label>
+                <input name='username' type='text' placeholder='Username' onChage={handleChange} required/>
+              </div>
+            {isSignup && (
+              <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='phoneNumber'>Phone #</label>
+                <input name='phoneNumber' type='text' placeholder='Phone Number' onChage={handleChange} required/>
+              </div>
+            )}
+            {isSignup && (
+              <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='avatarURL'>Avatar URL</label>
+                <input name='avatarURL' type='text' placeholder='Avatar URL' onChage={handleChange} required/>
+              </div>
+            )}
+            <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='password'>Password</label>
+                <input name='password' type='password' placeholder='Password' onChage={handleChange} required/>
+            </div>
+            {isSignup && (
+              <div className='auth__form-container_fields-content_input'>
+                <label htmlFor='confirmPassword'>Confirm Password</label>
+                <input name='confirmPassword' type='password' placeholder='Confirm Password' onChage={handleChange} required/>
+              </div>
+            )}
+          </form>
+          <div className='auth__form-container_fields-account'>
+            <p>
+              {isSignup? "Already have an account? " : "Don't have an account? "}
+              <span onClick={switchMode}>
+                {isSignup? 'Sign In' : 'Sign Up'}
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
+      <div className='auth__form-container_image'>
+        <img src={signinImage} alt='sign in'/>
       </div>
     </div>
   )
