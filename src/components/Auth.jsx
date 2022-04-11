d5c1fdaca4c63f1393cd24f9d6f89efa1b56555d
Gino <ginoalodar@gmail.com>
2022-04-11 09:36:59 -0500

    completed the sign up page to create an object per user

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -4,10 +4,27 @@ import axios from 'axios';
 
 import signinImage from '../assets/signup.jpg';
 
+const initialState = {
+  fullName: '',
+  username: '',
+  password: '',
+  confirmPassword: '',
+  phoneNumber: '',
+  avatarURL: ''
+}
+
 const Auth = () => {
+  const [form, setForm] = useState(initialState);
   const [isSignup, setIsSignup] = useState(true);
 
-  const handleChange=()=>{}
+  const handleChange=(e)=>{
+    setForm({...form, [e.target.name]: e.target.value});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  }
 
   const switchMode = () =>{
     setIsSignup((prevIsSignup)=> !prevIsSignup);
@@ -18,39 +35,42 @@ const Auth = () => {
       <div className='auth__form-container_fields'>
         <div className='auth__form-container_fields-content'>
           <p>{isSignup? 'Sign Up' : 'Sign In'}</p>
-          <form onSubmit={()=>{}}>
+          <form onSubmit={handleSubmit}>
             {isSignup && (
               <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='fullName'>Full Name</label>
-                <input name='fullName' type='text' placeholder='Full Name' onChage={handleChange} required/>
+                <input name='fullName' type='text' placeholder='Full Name' onChange={handleChange} required/>
               </div>
             )}
             <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='username'>Username</label>
-                <input name='username' type='text' placeholder='Username' onChage={handleChange} required/>
+                <input name='username' type='text' placeholder='Username' onChange={handleChange} required/>
               </div>
             {isSignup && (
               <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='phoneNumber'>Phone #</label>
-                <input name='phoneNumber' type='text' placeholder='Phone Number' onChage={handleChange} required/>
+                <input name='phoneNumber' type='text' placeholder='Phone Number' onChange={handleChange} required/>
               </div>
             )}
             {isSignup && (
               <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='avatarURL'>Avatar URL</label>
-                <input name='avatarURL' type='text' placeholder='Avatar URL' onChage={handleChange} required/>
+                <input name='avatarURL' type='text' placeholder='Avatar URL' onChange={handleChange} required/>
               </div>
             )}
             <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='password'>Password</label>
-                <input name='password' type='password' placeholder='Password' onChage={handleChange} required/>
+                <input name='password' type='password' placeholder='Password' onChange={handleChange} required/>
             </div>
             {isSignup && (
               <div className='auth__form-container_fields-content_input'>
                 <label htmlFor='confirmPassword'>Confirm Password</label>
-                <input name='confirmPassword' type='password' placeholder='Confirm Password' onChage={handleChange} required/>
+                <input name='confirmPassword' type='password' placeholder='Confirm Password' onChange={handleChange} required/>
               </div>
             )}
+            <div className='auth__form-container_fields-content_button'>
+              <button>{isSignup? 'Sign Up': 'Sign In'}</button>
+            </div>
           </form>
           <div className='auth__form-container_fields-account'>
             <p>
