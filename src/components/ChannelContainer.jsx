0277e8cac4d6a13f2309b37169b3d75bc52e9dd6
Gino <ginoalodar@gmail.com>
2022-04-13 01:16:57 -0500

    Created the channel container and a random test channel

--- src/components/ChannelContainer.jsx
+++ src/components/ChannelContainer.jsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Channel, useChatContext} from 'stream-chat-react';
 
-const ChannelContainer = () => {
+import {ChannelInner, CreateChannel, EditChannel, TeamMessage} from './';
+
+const ChannelContainer = ({isCreating, setIsCreating, isEditing, setIsEditing, createType}) => {
+  const {channel} = useChatContext();
+
+  if(isCreating){
+    return(
+      <div className='channel__container'>
+        <CreateChannel createType={createType} setIsCreating={setIsCreating}/>
+      </div>
+    )
+  }
+
+  if(isEditing){
+    return(
+      <div className='channel__container'>
+        <EditChannel setIsEditing={setIsEditing}/>
+      </div>
+    )
+  }
+
+  const EmptyState = () =>(
+    <div className='channel-empty__container'>
+      <p className='channel-empty__first'>Start of Chat History</p>
+      <p className='channel-empty__second'>Send attachments or links</p>
+    </div>
+  )
+
   return (
-    <div>
-      ChannelContainer
+    <div className='channel__container'>
+      <Channel EmptyStateIndicator={EmptyState} Message={(messageProps, i)=> <TeamMessage key={i} {...messageProps}/>}>
+        <ChannelInner setIsEditing={setIsEditing}/>
+      </Channel>
     </div>
   );
 }
